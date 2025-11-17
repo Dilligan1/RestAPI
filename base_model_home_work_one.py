6ebaa9c780126c18e7c014783c148c2574e188f8
qa-dilligan <dilligan007@mail.ru>
2025-11-17 16:53:33 +0300

    new req

--- base_model_home_work_one.py
+++ base_model_home_work_one.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel
-
+from pydantic import BaseModel, Field, field_validator
+from typing import List
+from datetime import date
 
 # Задание 1 Создание базовой модели Pydantic
 
@@ -24,3 +25,37 @@ response = {
 user_list = ItemList(**response)
 
 print(user_list)
+
+
+
+
+
+class Item(BaseModel):
+    name: str
+    price: float = Field(gt=0)  # цена должна быть больше 0
+
+class Order(BaseModel):
+    customer_name: str = Field(min_length=1)
+    order_date: date
+    items: List[Item]
+
+    @field_validator("order_date")
+    def check_in_data_today(cls, value):
+        if value < date.today():
+            raise ValueError("Дата в прошлом")
+        return value
+
+
+# Пример данных
+order_data = {
+    "customer_name": "Alex",
+    "order_date": "2023-12-31",
+    "items": [
+        {"name": "Laptop", "price": 1500},
+        {"name": "Smartphone", "price": 800}
+    ]
+}
+
+
+order = Order(**order_data)
+print(order)
